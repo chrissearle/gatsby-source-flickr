56ddc22194833432db705394fa7ff0a960d46397
Chris Searle <chris@chrissearle.org>
2021-01-04 11:38:35 +0100

    Update dependencies

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,4 +1,5 @@
-require("@babel/polyfill");
+require("core-js/stable");
+require("regenerator-runtime/runtime");
 const fetch = require("node-fetch");
 const queryString = require("query-string");
 
